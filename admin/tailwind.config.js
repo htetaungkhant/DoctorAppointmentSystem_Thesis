f23fdd3ed8737cde55925787cd16571e1a66381f
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-03-06 01:27:54 +0700

    added shadcn checkbox

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -5,12 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       gridTemplateColumns:{
         'auto':'repeat(auto-fill, minmax(200px, 1fr))'
       },
       colors:{
-        'primary':'#5F6FFF'
+        'border': "hsl(var(--border))",
+        'input': "hsl(var(--input))",
+        'ring': "hsl(var(--ring))",
+        'background': "hsl(var(--background))",
+        'foreground': "hsl(var(--foreground))",
+        'primary': {
+            DEFAULT: "hsl(var(--primary))",
+            foreground: "hsl(var(--primary-foreground))",
+          },
+        // 'primary':'#5F6FFF',
       }
     },
   },
